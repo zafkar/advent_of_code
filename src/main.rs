5d05de82b6f943e0a9c4ceb379814efde5310df0
Zif <zifgreyf@gmail.com>
2024-11-23 23:49:57 +0100

    more generic stuff

--- src/main.rs
+++ src/main.rs
@@ -6,12 +6,13 @@ const ADVENT_NUM: &str = "1";
 fn main() -> Result<(), Box<dyn Error>> {
     let file = load_data(ADVENT_NUM, "input.txt")?;
     for line in file.lines() {
-        let line = line?;
-        println!("{line}");
+        let row: Row = line?.parse()?;
+        println!("{row:?}");
     }
     Ok(())
 }
 
+#[derive(Debug)]
 struct Row;
 
 impl FromStr for Row {
