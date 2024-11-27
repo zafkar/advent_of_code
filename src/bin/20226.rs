6f704af12e9ee302b7d991a4e559092f07d6cb33
Zif <zifgreyf@gmail.com>
2024-11-27 13:46:08 +0100

    Done

---
+++ src/bin/20226.rs
@@ -0,0 +1,23 @@
+use advent_of_code::load_data;
+use itertools::Itertools;
+use std::{
+    error::Error,
+    fmt::Display,
+    io::{BufRead, Read},
+    str::FromStr,
+};
+
+const ADVENT_NUM: &str = "20226";
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let mut file = load_data(ADVENT_NUM, "sample.txt")?;
+    let mut data = String::new();
+    file.read_to_string(&mut data)?;
+    for i in 0..data.len() - 14 {
+        if data[i..i + 14].chars().all_unique() {
+            println!("{}", i + 14);
+            break;
+        }
+    }
+    Ok(())
+}
