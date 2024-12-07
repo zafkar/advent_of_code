9246674e60d2f1da6f31ca1d32f232a7f68b67c9
Zif <zifgreyf@gmail.com>
2024-12-07 11:32:01 +0100

    init

---
+++ src/bin/2024/7.rs
@@ -0,0 +1,34 @@
+use advent_of_code::load_data;
+use std::{error::Error, fmt::Display, io::BufRead, str::FromStr};
+
+const ADVENT_NUM: &str = "2024/7";
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let file = load_data(ADVENT_NUM, "sample.txt")?;
+    for line in file.lines() {
+        let row: Row = line?.parse()?;
+        println!("{row:?}");
+    }
+    Ok(())
+}
+
+#[derive(Debug)]
+struct Row;
+
+impl FromStr for Row {
+    type Err = GenericParseError;
+    fn from_str(_: &str) -> Result<Self, Self::Err> {
+        Ok(Row)
+    }
+}
+
+#[derive(Debug)]
+struct GenericParseError(String);
+
+impl Display for GenericParseError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{:?}", self)
+    }
+}
+
+impl Error for GenericParseError {}
